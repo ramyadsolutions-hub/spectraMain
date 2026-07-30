e766612c634086e6d0c74019ee7e56ae3045a4f8
ramyaa <ramyad.solutions@gmail.com>
2026-07-30 20:20:04 +0530

    made changes in main

---
+++ Main_v1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Testing the rebase main");
+
+Console.WriteLine("Main website");
+
